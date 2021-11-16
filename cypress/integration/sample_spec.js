898129d94562f5facad0e95a81d4f362fda1d450
Yudo Kartiko <yudoworks@gmail.com>
2021-11-16 21:01:57 +0700

    multipe assertion test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -26,4 +26,16 @@ describe("My First Test", () => {
 
     cy.url().should("include", "/commands/actions");
   });
+
+  it("Gets, types and asserts", () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+
+    cy.url().should("include", "/commands/actions");
+
+    cy.get(".action-email")
+      .type("fake@mail.com")
+      .should("have.value", "fake@mail.com");
+  });
 });
