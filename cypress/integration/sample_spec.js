9622feb82e60a09e1bfdea154e9687c5db7f03ae
Yudo Kartiko <yudoworks@gmail.com>
2021-11-16 20:44:34 +0700

    visit page test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,4 +2,8 @@ describe("My First Test", () => {
   it("Does not to much", () => {
     expect(true).to.equal(true);
   });
+
+  it("Visit the Kitchen Sink", () => {
+    cy.visit("https://example.cypress.io");
+  });
 });
